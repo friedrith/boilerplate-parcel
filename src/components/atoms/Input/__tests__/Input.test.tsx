984b2e27ba5006750f376772703840e6c52a73a2
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-03-24 08:46:30 -0400

    refactor: fix lint and format

--- src/components/atoms/Input/__tests__/Input.test.tsx
+++ src/components/atoms/Input/__tests__/Input.test.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import Input from '..'
 import buildRenderTest from '~/config/jest/buildRenderTest'
 
--- src/components/organisms/LanguageSwitcher/__tests__/LanguageSwitcher.test.tsx
+++ src/components/organisms/LanguageSwitcher/__tests__/LanguageSwitcher.test.tsx
@@ -1,23 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import LanguageSwitcher from '..'
-import mockWrapper from '~/src/__mocks__/mockWrapper'
 import buildRenderTest from '~/config/jest/buildRenderTest'
 
 const renderTest = buildRenderTest(LanguageSwitcher, {}, {})
 
 describe('LanguageSwitcher', () => {
   it('should render english option', () => {
-    const wrapper = mockWrapper().withI18n().build()
-
-    render(<LanguageSwitcher />, { wrapper })
+    renderTest()
 
     expect(screen.queryByText('English')).toBeInTheDocument()
   })
 
   it('should render français option', () => {
-    const wrapper = mockWrapper().withI18n().build()
-
-    render(<LanguageSwitcher />, { wrapper })
+    renderTest()
 
     expect(screen.queryByText('Français')).toBeInTheDocument()
   })
